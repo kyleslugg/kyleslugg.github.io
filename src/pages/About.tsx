c495965188fcba50c6909cf17169445cc7e6cfeb
Kyle Slugg <kyle.slugg@gmail.com>
2023-07-09 14:01:17 -0400

    Updated about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,30 +4,26 @@ export default function About() {
   return (
     <Card sx={{ fontFamily: 'Arial' }}>
       <div className="card">
-        <h2>🏗 CAUTION: Under Construction 🏗</h2>
         <h3>Welcome! I'm Kyle Slugg-Urbino.</h3>
         <p>
-          I blend policy analysis and data science to figure out how urban
-          governance can promote racial and economic justice, so that all New
-          Yorkers share in our city's prosperity.
+          I'm a software engineer working (mostly) in JavaScript/TypeScript and
+          Python to develop and improve open-source tools that render real-time
+          operational information about complex systems accessible to senior
+          decisionmakers, esp. those in city and metropolitan governments. I'm
+          currently working on walk routing tools to facilitate deeper
+          engagement between citizens and their environments. Reach out to talk
+          urban informatics, data-driven governance, and/or the beautiful and
+          complex interdependencies of urban life.
         </p>
         <p>
-          As a former translator, I am keenly aware of how making change depends
-          on turning messy facts and data into a strong, compelling narrative
-          that will drive policymakers to action. I take pride in my ability to
-          make complex topics understandable to non-technical decisionmakers as
-          well as the public, using both clear writing and compelling visuals.
-          Let's figure out how we can work together to make urban space function
-          better for all of us.
+          As a former translator and public servant, I am keenly aware of how
+          making change depends on turning messy facts and data into a strong,
+          compelling narrative that will drive policymakers to action. I take
+          pride in my ability to make complex topics understandable to
+          non-technical decisionmakers as well as the public. Let's figure out
+          how we can work together to make urban space function better for all
+          of us.
         </p>
-        <p>My areas of expertise include:</p>
-        <ul>
-          <li>Statistical modeling using Stata and Python</li>
-          <li>Geospatial Analysis and Analytic Mapping</li>
-          <li>Real Estate Market Analysis</li>
-          <li>Data Visualization</li>
-          <li>Policy Writing</li>
-        </ul>
       </div>
     </Card>
   );
