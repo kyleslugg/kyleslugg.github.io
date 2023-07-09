7cdb80b14f93246ed772c7beb8158a68da166d32
Kyle Slugg <kyle.slugg@gmail.com>
2023-07-09 16:45:16 -0400

    Added project blurbs and some (but not all) links to Work page.

---
+++ src/components/postLede.tsx
@@ -0,0 +1,46 @@
+import { Card } from '@mui/material';
+export interface BlurbInfo {
+  date: string;
+  title: string;
+  imgSrc: string;
+  blurbText: string;
+  fulltextUri: string;
+}
+export default function PostLede(props: {
+  data: BlurbInfo;
+  photoDirectory: string;
+}) {
+  const { date, title, imgSrc, blurbText, fulltextUri } = props.data;
+  const { photoDirectory } = props;
+
+  return (
+    <Card
+      className="postBlurbHolder"
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 2fr',
+        gridColumnGap: '10px',
+        padding: '5px',
+        width: '100%',
+        height: '175px',
+        boxShadow: '0pt 0pt 2pt 2pt #00000022'
+      }}
+    >
+      <div
+        className="workImgHolder"
+        style={{
+          backgroundImage: `Url(${photoDirectory.concat(imgSrc)})`
+        }}
+      ></div>
+      <div className="postBlurbText">
+        <h4>{title}</h4>
+        <p>
+          {blurbText}{' '}
+          <a href={fulltextUri} target="_blank" style={{ marginRight: '10px' }}>
+            Read More &#10142;{' '}
+          </a>
+        </p>
+      </div>
+    </Card>
+  );
+}
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,10 +1,21 @@
-import Card from '@mui/material/Card';
+import { Card, Container, Grid } from '@mui/material';
+import PostLede, { BlurbInfo } from '../components/postLede';
+import blurbs from '../assets/markdown/posts/blurbs.json';
+import '../styles/Posts.scss';
 
 export default function Work() {
+  const blurbData: BlurbInfo[] = JSON.parse(JSON.stringify(blurbs));
+  const photoDirectory = '/post_images/';
+  const blurbElements = blurbData.map((el) => (
+    <PostLede data={el} photoDirectory={photoDirectory} />
+  ));
+
   return (
     <Card sx={{ fontFamily: 'Arial' }}>
       <div className="card">
-        <h2>🏗 CAUTION: Under Construction 🏗</h2>
+        <h3>The Latest</h3>
+        <h4>🏗 Hard Hats Advised: Some portions under construction 🏗</h4>
+        <Grid>{blurbElements}</Grid>
       </div>
     </Card>
   );
